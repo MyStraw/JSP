10c69a6238ad77ebd62bb692581d42d2861aa26c
MyStraw <kikongdosa@gmail.com>
2023-05-19 17:57:53 +0900

    수업끝

--- MustHaveJSP/src/main/java/common/JDBConnect.java
+++ MustHaveJSP/src/main/java/common/JDBConnect.java
@@ -7,17 +7,18 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.Timestamp;
 
-public class JDBConnect {
-
-	public static void main(String[] args) {
-		JDBConnect jdbconnect = new JDBConnect();
-
-	}
+public class JDBConnect {	
 
 	public Connection con;
 	public Statement stmt;
 	public PreparedStatement psmt;
 	public ResultSet rs;
+	
+//	private String driver ="com.mysql.cj.jdbc.Driver";
+//	private String url = "jdbc:mysql://localhost:3306/musthave";
+//	private String id = "musthave";
+//	private String pwd = "tiger";
+			
 
 	public JDBConnect() {
 		try {
@@ -34,10 +35,6 @@ public class JDBConnect {
 		}
 	}
 
-	public JDBConnect(String driver, String url, String id, String pwd) {
-
-	}
-
 	public void close() {
 		try {
 			if (rs != null)
@@ -50,6 +47,39 @@ public class JDBConnect {
 				con.close();
 
 			System.out.println("JDBC 자원 해제");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		JDBConnect jdbconnect = new JDBConnect();
+		jdbconnect.showTable();
+		jdbconnect.close();
+	}
+
+	public JDBConnect(String driver, String url, String id, String pwd) {
+
+	}
+
+	public void showTable() {
+
+		try {
+			String query = "select * from board";
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(query);
+
+			while (rs.next()) {
+				int num = rs.getInt(1);
+				String title = rs.getString(2);
+				String content = rs.getString(3);
+				String id = rs.getString(4);
+				Timestamp postdate = rs.getTimestamp(5);
+				int visitcount = rs.getInt(6);
+
+				System.out.printf("%5d\t%s\t%s\t%8s\t%s\t%d\n", num, title, content, id, postdate, visitcount);
+
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
