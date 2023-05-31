60fab60e1ba98960425ed4c2729686bfd0309fd6
MyStraw <kikongdosa@gmail.com>
2023-05-31 17:58:16 +0900

    5월 31일

--- MustHaveJSP/src/main/java/membership/MemberDTO.java
+++ MustHaveJSP/src/main/java/membership/MemberDTO.java
@@ -1,12 +1,29 @@
 package membership;
 
-public class MemberDTO {
+import java.io.Serializable;
+
+public class MemberDTO implements Serializable { //마크인터페이스. 시리얼라이즈 직렬화 해서 파일에 쓰고 읽고 할수있게 마킹. 이런 마킹이 붙어있어야 읽고 쓰고 가능.
+		
+	private static final long serialVersionUID = 1L; //디스크에 특정 객체 write 하고 읽을때 검증목적. 쓰거나 읽을 목적이 없으면 이거 필요없다.
 	
+	//멤버 변수 선언
 	private String id;
 	private String pass;
 	private String name;
 	private String regidate;
 	
+	public MemberDTO() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public MemberDTO(String id, String pass, String name, String regidate) {
+		// TODO Auto-generated constructor stub
+		this.id=id;
+		this.pass=pass;
+		this.name=name;
+		this.regidate=regidate;
+	}
+
 	public String getId() {
 		return id;
 	}
---
+++ MustHaveJSP/src/main/java/servlet/DirectServletPrint.java
@@ -0,0 +1,48 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/13Servlet/DirectServletPrint.do")
+public class DirectServletPrint extends HttpServlet { //1.web.xml 하든 2.어노테이션 붙이든 둘중하나. 서블릿 구동하려면.
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter writer = resp.getWriter();
+
+		writer.println("<html>");
+		writer.println("<head><title>DirectServletPrint</title></head>");
+		writer.println("<body>");
+		writer.println("<h2>서블릿에서 직접 출력합니다.</h2>");
+		writer.println("<p>jsp로 포워드하지 않습니다.</p>");
+		writer.println("</body>");
+		writer.println("</html>");
+
+		writer.close();
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException { //포스트맨 put 으로 바꾸면 이게 나와
+
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter writer = resp.getWriter();
+
+		writer.println("<html>");
+		writer.println("<head><title>DirectServletPrint</title></head>");
+		writer.println("<body>");
+		writer.println("<h2>서블릿에서 직접 출력합니다.</h2>");
+		writer.println("<p>jsp로 포워드하지 않습니다.</p>");
+		writer.println("</body>");
+		writer.println("</html>");
+
+		writer.close();
+	}
+}
